9d07b4d575ff232a6e525749b9d3c63b826af8b0
ColJops <94141667+ColJops@users.noreply.github.com>
2022-04-23 17:01:41 +0200

    zmiany

---
+++ start.py
@@ -0,0 +1,20 @@
+print('Hello world')
+print("Druga instrukcja")
+x = 3 #integer
+f = 3.15 #float
+name = "Daniel" #string
+arr = ["apple", "banana", "banana"] #list
+dictionary = {"imie" : "Daniel", "age" : 36} #dict
+logic = True #boolean
+z = x + f
+
+print(x)
+print(f)
+print(name)
+print(z)
+
+sumInteger = (int(x) + int(f))
+print(sumInteger)
+print(arr)
+print(arr[1])
+print(dictionary)
